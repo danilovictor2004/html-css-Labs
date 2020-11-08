dc3eaeab1526d986cdc71f5f01dba532421f98ef
Danillo Victtor <danilo_victor2004@hotmail.com>
2020-11-08 14:49:33 -0300

    commit update

--- flexblog/js/script.js
+++ flexblog/js/script.js
@@ -1,18 +1,3 @@
-function initScrollSuave(){
-  const linksInternos = document.querySelectorAll('.menu-nav a[href^="#"]');
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+import initScrollSuave from './modules/scrollSuave.js';
 
-  function scrollToSection(event){
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-  }
-}
 initScrollSuave();
